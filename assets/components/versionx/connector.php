7ab1d5893e4805b214a03cbc01e12f48148651a8
Mark Hamstra <markieham@gmail.com>
2011-01-14 14:23:26 +0100

    Use core path from the versionx namespace

--- assets/components/versionx/connector.php
+++ assets/components/versionx/connector.php
@@ -24,9 +24,9 @@
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
-/*$modx = new modX();  
-$modx->initialize('mgr'); */
-$corePath = $modx->config['core_path'].'components/versionx/';
+
+$namespace = $modx->getObject('modNamespace', 'versionx');
+$corePath = $namespace->get('path');
 
 $modx->addPackage('versionx', $corePath.'model/');
 
